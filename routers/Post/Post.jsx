11bdf505730f587a2338f15f6ce8a24abfaa057d
Тимофей <FRedeaX@Yandex.ru>
2022-01-19 17:02:46 +0500

    0.0.1

--- routers/Post/Post.jsx
+++ routers/Post/Post.jsx
@@ -1,12 +1,8 @@
-/* eslint-disable no-unused-vars */
-
-/* eslint-disable arrow-body-style */
 import { gql } from "@apollo/client";
 import classNames from "classnames";
-import { useEffect } from "react";
 
 import Article from "../../components/Article/Article";
-import { postGQL } from "../../components/Posts/PostsRoot";
+// import { useRef } from "react";
 import Button from "../../components/UI/Button/Button";
 import Icon from "../../components/UI/Icon/Icon";
 import { columnsBlockGQL } from "../../components/blocks/Columns/Columns";
@@ -25,46 +21,26 @@ import { spacerBlockGQL } from "../../components/blocks/Spacer/Spacer";
 import { tableBlockGQL } from "../../components/blocks/Table/Table";
 import { verseBlockGQL } from "../../components/blocks/Verse/Verse";
 // import { useOnScreen } from "../../helpers/frontend";
+// import { createMarkup } from "~/helpers";
 import classes from "./Post.module.css";
-import Offer from "./offer/Offer";
 import usePost from "./usePost";
 
 export const Post = ({
-  id,
-  href,
   title,
+  categories,
+  href,
   image,
   blocks,
-  categories,
   isPreview = false,
 }) => {
-  // const router = useRouter();
   // const ref = useRef();
   // const { isOnScreen } = useOnScreen(null, "0px", 0.5);
-  const { hendleOffers, offerList } = usePost();
+  const { hendeToTop } = usePost();
 
-  useEffect(() => {
-    hendleOffers(id);
-  }, [hendleOffers, id]);
-
-  // const [ref, { isVisible }] = useIntersectionObserver({ threshold: 0.5 });
-  // useEffect(() => {
-  //   if (isVisible) {
-  //     const { nextPost } = offerList[offerList.length - 1];
-  //     hendleOffers(nextPost.id);
-  //   }
-  // }, [hendleOffers, offerList, isVisible]);
-
-  // useEffect(() => {
-  //   if (isVisible) {
-  //     const { nextPost } = offerList[offerList.length - 1];
-  //     hendleOffers(nextPost.id);
-  //   }
-  // }, [hendleOffers, offerList, isVisible]);
-
+  // const feed = [{post:{}, readMore: []}, {post:{}, readMore: []}]
   return (
     <>
-      {/* <Button
+      <Button
         className={classNames(
           classes["button_to-top"],
           // classes[`button_to-top_${isOnScreen}`],
@@ -72,7 +48,7 @@ export const Post = ({
         iconLeft={<Icon direction="top" weight="small" />}
         onClick={hendeToTop}>
         <span className={classes["button_to-top_text"]}>Наверх</span>
-      </Button> */}
+      </Button>
 
       <Article
         title={title}
@@ -82,34 +58,30 @@ export const Post = ({
         href={href}
         image={image}
       />
-
-      {offerList.length > 0 && (
-        <div className={classes.feed}>
-          <Offer
-            postListByCategory={offerList[0].postListByCategory}
-            similarPostList={offerList[0].similarPostList}
-            categories={categories.nodes}
-            // nextPost={nextPost}
-          />
-        </div>
-      )}
     </>
   );
 };
 
-export const GET_MINIMUM_DATA_FOR_OFFER = gql`
-  query GET_MINIMUM_DATA_FOR_OFFER($id: ID!) {
-    post(id: $id) {
+export const FETCH_ARTICLE = gql`
+  query fetchArticle($id: ID!, $type: PostIdType, $isPreview: Boolean) {
+    post(id: $id, idType: $type, asPreview: $isPreview) {
       categories {
         nodes {
-          termTaxonomyId
+          id
+          name
+          uri
         }
       }
-      postsFields {
-        keywords
+      content
+      excerpt
+      featuredImage {
+        node {
+          sourceUrl(size: THUMBNAIL)
+        }
       }
-      postId
-      date
+      id
+      link
+      title
     }
   }
 `;
@@ -135,8 +107,22 @@ export const GET_POST_CONTENT_BY_BLOCKS = gql`
         ...tableBlockGQL
         ...verseBlockGQL
       }
-      ...postGQL
-      postId
+      categories {
+        nodes {
+          id
+          name
+          uri
+        }
+      }
+      excerpt
+      featuredImage {
+        node {
+          sourceUrl(size: THUMBNAIL)
+        }
+      }
+      id
+      link
+      title
     }
   }
   ${paragraphBlockGQL.fragments}
@@ -154,5 +140,4 @@ export const GET_POST_CONTENT_BY_BLOCKS = gql`
   ${headingBlockGQL.fragments}
   ${tableBlockGQL.fragments}
   ${verseBlockGQL.fragments}
-  ${postGQL.fragments}
 `;
