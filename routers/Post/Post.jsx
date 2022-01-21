159c3476fbf450f3b0e56c7169ffa4f6f5bf2e0b
Тимофей <FRedeaX@Yandex.ru>
2022-01-21 11:39:54 +0500

    0.0.2

--- routers/Post/Post.jsx
+++ routers/Post/Post.jsx
@@ -1,7 +1,10 @@
 import { gql } from "@apollo/client";
 import classNames from "classnames";
+import { useEffect } from "react";
 
 import Article from "../../components/Article/Article";
+import GroupCards from "../../components/Posts/GroupCards/GroupCards";
+import { postGQL } from "../../components/Posts/PostsRoot";
 // import { useRef } from "react";
 import Button from "../../components/UI/Button/Button";
 import Icon from "../../components/UI/Icon/Icon";
@@ -26,11 +29,12 @@ import classes from "./Post.module.css";
 import usePost from "./usePost";
 
 export const Post = ({
-  title,
-  categories,
   href,
+  title,
   image,
   blocks,
+  offers,
+  categories,
   isPreview = false,
 }) => {
   // const ref = useRef();
@@ -49,6 +53,12 @@ export const Post = ({
   //   }
   // }, [hendleOffers, offerList, isVisible]);
 
+  // useEffect(() => {
+  //   console.log("2");
+  // }, []);
+
+  // eslint-disable-next-line no-console
+  console.log(offerList);
   // const feed = [{post:{}, readMore: []}, {post:{}, readMore: []}]
   return (
     <>
@@ -85,30 +95,6 @@ export const Post = ({
   );
 };
 
-export const FETCH_ARTICLE = gql`
-  query fetchArticle($id: ID!, $type: PostIdType, $isPreview: Boolean) {
-    post(id: $id, idType: $type, asPreview: $isPreview) {
-      categories {
-        nodes {
-          id
-          name
-          uri
-        }
-      }
-      content
-      excerpt
-      featuredImage {
-        node {
-          sourceUrl(size: THUMBNAIL)
-        }
-      }
-      id
-      link
-      title
-    }
-  }
-`;
-
 export const GET_POST_CONTENT_BY_BLOCKS = gql`
   query fetchArticle($id: ID!, $type: PostIdType, $isPreview: Boolean) {
     post(id: $id, idType: $type, asPreview: $isPreview) {
@@ -130,22 +116,7 @@ export const GET_POST_CONTENT_BY_BLOCKS = gql`
         ...tableBlockGQL
         ...verseBlockGQL
       }
-      categories {
-        nodes {
-          id
-          name
-          uri
-        }
-      }
-      excerpt
-      featuredImage {
-        node {
-          sourceUrl(size: THUMBNAIL)
-        }
-      }
-      id
-      link
-      title
+      ...postGQL
     }
   }
   ${paragraphBlockGQL.fragments}
@@ -163,4 +134,5 @@ export const GET_POST_CONTENT_BY_BLOCKS = gql`
   ${headingBlockGQL.fragments}
   ${tableBlockGQL.fragments}
   ${verseBlockGQL.fragments}
+  ${postGQL.fragments}
 `;
