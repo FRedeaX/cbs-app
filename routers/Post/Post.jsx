d65b8fddfbdf360737ba7b83e66e68812cf06831
Тимофей <FRedeaX@Yandex.ru>
2022-02-11 17:41:54 +0500

    Предложение похожих записей под постом

--- routers/Post/Post.jsx
+++ routers/Post/Post.jsx
@@ -3,11 +3,9 @@
 /* eslint-disable arrow-body-style */
 import { gql } from "@apollo/client";
 import classNames from "classnames";
-import { useRouter } from "next/router";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 
 import Article from "../../components/Article/Article";
-import InfiniteScrolling from "../../components/InfiniteScrolling/InfiniteScrolling";
 import { postGQL } from "../../components/Posts/PostsRoot";
 import Button from "../../components/UI/Button/Button";
 import Icon from "../../components/UI/Icon/Icon";
@@ -26,7 +24,6 @@ import { separatorBlockGQL } from "../../components/blocks/Separator/Separator";
 import { spacerBlockGQL } from "../../components/blocks/Spacer/Spacer";
 import { tableBlockGQL } from "../../components/blocks/Table/Table";
 import { verseBlockGQL } from "../../components/blocks/Verse/Verse";
-import { useIntersectionObserver } from "../../helpers/frontend/hooks";
 // import { useOnScreen } from "../../helpers/frontend";
 import classes from "./Post.module.css";
 import Offer from "./offer/Offer";
@@ -41,6 +38,7 @@ export const Post = ({
   categories,
   isPreview = false,
 }) => {
+  // const router = useRouter();
   // const ref = useRef();
   // const { isOnScreen } = useOnScreen(null, "0px", 0.5);
   const { hendleOffers, offerList } = usePost();
@@ -58,16 +56,11 @@ export const Post = ({
   // }, [hendleOffers, offerList, isVisible]);
 
   // useEffect(() => {
-  //   if (offerList.length > 0 && isVisible) {
-  //     router.push(
-  //       offerList[offerList.length - 1].nextPost.uri,
-  //       offerList[offerList.length - 1].nextPost.uri,
-  //       { scroll: false, shallow: true },
-  //     );
+  //   if (isVisible) {
+  //     const { nextPost } = offerList[offerList.length - 1];
+  //     hendleOffers(nextPost.id);
   //   }
-
-  //   console.log(`The component is ${isVisible ? "visible" : "not visible"}.`);
-  // }, [isVisible]);
+  // }, [hendleOffers, offerList, isVisible]);
 
   return (
     <>
@@ -116,6 +109,7 @@ export const GET_MINIMUM_DATA_FOR_OFFER = gql`
         keywords
       }
       postId
+      date
     }
   }
 `;
