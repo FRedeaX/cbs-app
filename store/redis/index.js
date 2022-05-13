1c274397df982a309f889960aca7df30e3113539
Timofey <FRedeaX@Yandex.ru>
2022-05-13 09:44:25 +0500

    1.4.1

--- store/redis/index.js
+++ store/redis/index.js
@@ -1,36 +1,18 @@
 import { Tedis } from "tedis";
 
 // import { errorHelper } from "../../helpers/errorHendler";
-
-const clientRedis = null;
+let client = null;
 
 try {
-  typeof window === "undefined" &&
-  new Tedis({
-    host: `${process.env.REDIS_HOST}`,
-    // host: `${process.env.REDIS_HOST_DEV}`,
-    port: `${process.env.REDIS_PORT}`,
-  });  
+  if (typeof window === "undefined") {
+    client = new Tedis({
+      host: `${process.env.REDIS_HOST}`,
+      // host: `${process.env.REDIS_HOST_DEV}`,
+      port: `${process.env.REDIS_PORT}`,
+    });
+  }
 } catch (error) {
   console.error(error);
 }
-
+const clientRedis = client;
 export default clientRedis;
-// (async () => {
-//   await clientRedis
-//     .get("post")
-//     .then(() => console.log("Redis started"))
-//     .catch((e) => console.error("Redis error", e));
-// })();
-// export let clientRedis;
-
-// errorHelper(() => {
-//   clientRedis =
-//     typeof window === "undefined" &&
-//     new Tedis({
-//       host: `${process.env.REDIS_HOST}`,
-//       port: `${process.env.REDIS_PORT}`,
-//     });
-// });
-
-// console.log(clientRedis);
