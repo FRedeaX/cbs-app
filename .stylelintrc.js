ad36561c8c111c5682f54f5286b34b96ee48fb84
Timofey <FRedeaX@Yandex.ru>
2022-08-16 23:47:37 +0500

    Заменяет библиотеку для сортировки стилей

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,10 @@
 module.exports = {
   extends: [
     "stylelint-config-standard",
-    "stylelint-config-rational-order",
+    "stylelint-config-clean-order",
     "stylelint-prettier/recommended",
   ],
-  plugins: ["stylelint-order"],
   rules: {
-    "order/order": [
-      ["custom-properties", "declarations"],
-      // {
-      // 	"disableFix": true
-      // }
-    ],
     "color-function-notation": "legacy",
     "selector-class-pattern": "[a-z]+|_([a-z]+)?([a-z]+[A-Z][a-z]+)?",
   },
