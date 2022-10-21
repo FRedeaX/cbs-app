ff8a3a5776e840f727620ef165817fe2823a9e29
Timofei <FRedeaX@Yandex.ru>
2022-10-21 12:29:02 +0500

    Добавляет типы для `createMarkup`

--- helpers/createMarkup.ts
+++ helpers/createMarkup.ts
@@ -1,5 +1,9 @@
-function createMarkup(text: string | null) {
-  if (text === null) return undefined;
+import { ReactNode } from "react";
+
+type createMarkupResult = { __html: string } | undefined;
+
+function createMarkup(text: string | ReactNode | null): createMarkupResult {
+  if (text === null || typeof text !== "string") return undefined;
 
   return { __html: text };
 }
