d92ba20bab2e561829d7460dc476f1664d7907df
Timofei <FRedeaX@Yandex.ru>
2022-10-21 12:18:16 +0500

    Добавляет универсальные дженерики

---
+++ helpers/typings/utility-types.ts
@@ -0,0 +1,29 @@
+/**
+ * Возвращает тип без указанных свойств.
+ *
+ * @param T Исходный тип.
+ * @param U Свойства которые необходимо удалить.
+ */
+export type Omit<T, U> = Pick<T, Exclude<keyof T, U>>;
+
+/**
+ * Возвращает тип который может быть null.
+ */
+export type Nullable<T> = T | null;
+
+/**
+ * Возвращает тип который может быть undefined.
+ */
+export type Maybe<T> = T | undefined;
+
+/**
+ * Возвращает тип в котором переданные ключи являются обязательными.
+ */
+export type Defaultize<TSource, TKeys extends keyof TSource> = TSource & {
+  [P in TKeys]-?: TSource[P];
+};
+
+/**
+ * Тип который ничего не возвращает.
+ */
+export type Void = () => void;
