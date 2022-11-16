627624e8ea3bdc40b372b0a087e256eb5f939f77
Timofei <FRedeaX@Yandex.ru>
2022-11-16 14:50:57 +0500

    Изменяет обязательные параметры

--- components/SEO/SEO.jsx
+++ components/SEO/SEO.jsx
@@ -8,10 +8,10 @@ const SEO = ({ title, description, image, video, url }) => {
   return (
     <Head>
       <title>{fullTitle}</title>
-      <meta name="description" content={description} />
+      {description && <meta name="description" content={description} />}
 
       <meta property="og:title" content={fullTitle} />
-      <meta property="og:description" content={description} />
+      {description && <meta property="og:description" content={description} />}
       {url && <meta property="og:url" content={url} />}
       {image && <meta property="og:image" content={image} />}
       {video &&
