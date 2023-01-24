fa02e906d5df3e225217b2b4b974a81202bee4d9
Timofei <FRedeaX@Yandex.ru>
2023-01-24 17:45:39 +0500

    Добавляет динамический импорт в Preview

--- pages/0.jsx
+++ pages/0.jsx
@@ -1,6 +1,16 @@
-import PreviewRender from "../components/Pages/Preview/Preview";
+import dynamic from "next/dynamic";
+
 import Layout from "../components/UI/Layout/Layout";
 import { getMenu } from "../helpers/backend";
+
+const PreviewRender = dynamic(
+  () =>
+    import("../components/Pages/Preview/Preview").then((res) => res.Preview),
+  {
+    ssr: false,
+    loading: () => "Загрузка компонента...",
+  },
+);
 
 const Preview = ({ menu, id, isPage }) => (
   <Layout menu={menu} size="m">
@@ -14,7 +24,7 @@ export async function getServerSideProps({ query }) {
     props: {
       menu,
       id: query.preview_id || query.p || query.page_id,
-      isPage: query.page_id,
+      isPage: !!query.page_id,
     },
   };
 }
