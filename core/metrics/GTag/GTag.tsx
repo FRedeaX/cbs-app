3c73188479e2801215565fec65222a4a35340f44
Timofei <FRedeaX@Yandex.ru>
2023-01-25 09:18:19 +0500

    Обновляет google аналитику

--- core/metrics/GTag/GTag.tsx
+++ core/metrics/GTag/GTag.tsx
@@ -1,21 +1,29 @@
 import Script from "next/script";
 
-type Gtag = (...args: unknown[]) => void;
+// type Gtag = (...args: unknown[]) => void;
 
 export const GTag = () =>
   process.env.NODE_ENV === "production" && (
-    <Script
-      id="sgt"
-      src="https://www.googletagmanager.com/gtag/js?id=G-SSD21VV9G7"
-      onLoad={() => {
-        window.dataLayer = window.dataLayer || [];
+    <>
+      <Script
+        id="sgt"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SSD21VV9G7"
+        // onLoad={() => {
+        //   window.dataLayer = window.dataLayer || [];
 
-        const gtag: Gtag = (...args) => {
-          window.dataLayer.push(args);
-        };
+        //   const gtag: Gtag = (...args) => {
+        //     window.dataLayer.push(args);
+        //   };
 
-        gtag("js", new Date());
-        gtag("config", "G-SSD21VV9G7");
-      }}
-    />
+        //   gtag("js", new Date());
+        //   gtag("config", "G-SSD21VV9G7");
+        // }}
+      />
+      <Script id="sgt-init">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-SSD21VV9G7');`}
+      </Script>
+    </>
   );
