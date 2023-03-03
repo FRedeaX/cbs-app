5aae598fde0ad3e221fd907252d9a3b2c2bbfcbd
Timofei <FRedeaX@Yandex.ru>
2023-03-03 17:46:38 +0500

    Добавляет компонент для красивой загрузки картинок

---
+++ components/Image/Image.tsx
@@ -0,0 +1,135 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Skeleton } from "@mui/material";
+import classNames from "classnames";
+import NextImage, { ImageProps as NextImageProps } from "next/future/image";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
+
+import classes from "./Image.module.css";
+
+const ANIMATION_DELAY_MS = 50;
+
+// const sxSkeleton: CSSProperties = {
+//   height: "100%",
+//   backgroundColor: "transparent",
+//   transform: "none",
+//   position: "absolute",
+// };
+
+type ImageProps = {
+  /**
+   * Дополнительный класс для обертки.
+   */
+  classNamePlaceholder?: string;
+} & NextImageProps;
+
+export const Image: FC<ImageProps> = ({
+  placeholder,
+  blurDataURL,
+  alt,
+  style,
+  className,
+  classNamePlaceholder,
+  width,
+  height,
+  src,
+  ...prop
+}) => {
+  const [isLoaded, setLoaded] = useState(false);
+  const [isFailed, setFailed] = useState(false);
+  const [needAnimation, setNeedAnimation] = useState(false);
+  const [canRemovePlaceholder, setCanRemovePlaceholder] = useState(false);
+
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const isRendered = useCallback(() => {
+    if (!imageRef.current) {
+      return false;
+    }
+    const { naturalWidth, naturalHeight } = imageRef.current;
+
+    return naturalWidth > 0 && naturalHeight > 0;
+  }, [imageRef]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!isLoaded && !isRendered()) {
+        setNeedAnimation(true);
+      }
+    }, ANIMATION_DELAY_MS);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isLoaded, isRendered, setNeedAnimation]);
+
+  useEffect(() => {
+    if (!isLoaded && imageRef.current && imageRef.current.complete) {
+      setLoaded(true);
+    }
+  }, [isLoaded]);
+
+  const handleOnAnimationEnd = useCallback(() => {
+    setCanRemovePlaceholder(true);
+  }, []);
+
+  const handleOnLoad = useCallback(() => {
+    setLoaded(true);
+  }, []);
+
+  const handleOnError = useCallback(() => {
+    setFailed(true);
+  }, []);
+
+  const imageClassNames = classNames(className, classes.image, {
+    [classes.image_loaded]: isLoaded,
+    [classes.image_loading]: needAnimation,
+    [classes.image_animated]: needAnimation && isLoaded,
+  });
+
+  const image = (
+    <NextImage
+      alt={alt}
+      aria-hidden={alt ? "false" : "true"}
+      src={isFailed ? "" : src}
+      className={imageClassNames}
+      width={width}
+      height={height}
+      onAnimationEnd={handleOnAnimationEnd}
+      onError={handleOnError}
+      onLoad={handleOnLoad}
+      {...prop}
+    />
+  );
+
+  if (placeholder === "blur" && blurDataURL !== undefined) {
+    return (
+      <div
+        style={style}
+        className={classNames(classes.placeholder, classNamePlaceholder)}>
+        {needAnimation && !canRemovePlaceholder && (
+          <>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              alt=""
+              aria-hidden="true"
+              src={blurDataURL}
+              className={classNames(className, classes.blur)}
+              width={width}
+              height={height}
+            />
+            <Skeleton
+              // sx={sxSkeleton}
+              className={classNames(className, classes.skeleton)}
+              animation="wave"
+              width={width}
+              height={height}
+            />
+          </>
+        )}
+        {image}
+      </div>
+    );
+  }
+
+  return image;
+};
