30d657cd34b8937cfab6777a68938a4123c8499f
Timofei <FRedeaX@Yandex.ru>
2023-03-06 10:39:58 +0500

    Изменяет разбиение для 3, 4 изображений

--- components/blocks/Gallery/utils/splitByLines.ts
+++ components/blocks/Gallery/utils/splitByLines.ts
@@ -4,15 +4,17 @@ type splitByLinesResult<T> = {
 };
 
 /**
+ * *1.5 - соотношение 2:3*
+ *
  * Разделяет массив на 2 подмассива по следующим условиям:
  *
  *  - array.legth <= 2 return { big: [0, 1], small: [] }
  *  - array.legth === 3
- *    - aspectRatio < 1 return { big: [0, 1, 2], small: [] }
- *    - aspectRatio >= 1 return { big: [0], small: [1, 2] }
+ *    - aspectRatio <= 1.5 return { big: [0, 1, 2], small: [] }
+ *    - aspectRatio > 1.5 return { big: [0], small: [1, 2] }
  *  - array.legth === 4
- *    - aspectRatio < 1 return { big: [0, 1, 2, 3], small: [] }
- *    - aspectRatio >= 1 return { big: [0], small: [1, 2, 3] }
+ *    - aspectRatio <= 1.5 return { big: [0, 1, 2, 3], small: [] }
+ *    - aspectRatio > 1.5 return { big: [0], small: [1, 2, 3] }
  *  - array.legth >= 5 return { big: [0, 1], small: [2, 3, 4, .. 9] }
  *
  * @param array Массив изображений.
@@ -23,7 +25,7 @@ export const splitByLines = <T>(
   aspectRatio: number,
 ): splitByLinesResult<T> => {
   if (array.length > 2 && array.length < 5) {
-    if (aspectRatio < 1) {
+    if (aspectRatio < 1.5) {
       return { big: array, small: [] };
     }
 
