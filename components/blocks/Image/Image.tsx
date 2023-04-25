9e0a31d34e7d5a9a9b00255f43fc05653b980522
Timofei <FRedeaX@Yandex.ru>
2023-04-25 12:19:37 +0500

    Рефакторит блок `Image`

---
+++ components/blocks/Image/Image.tsx
@@ -0,0 +1,108 @@
+import classNames from "classnames";
+import { FC, HTMLAttributes } from "react";
+
+import { useToggle } from "../../../helpers/frontend/hooks";
+import { Figure, FigureFigcaption } from "../../Figure";
+import {
+  Image as AnimatedImage,
+  ImageProps as IAnimatedImageProps,
+} from "../../Image/Image";
+import {
+  ImageViewer,
+  ImageViewerBody,
+  ImageViewerFigure,
+  ImageViewerHeader,
+} from "../../ImageViewer";
+import { ImageViewerButton } from "../../ImageViewer/components/Button/ImageViewer.Button";
+import { AlignImage } from "../utils/types";
+import classes from "./Image.module.css";
+
+type ImagePropsExtends = "alt" | "src" | "width" | "height" | "blurDataURL";
+
+export type ImageProps = {
+  /**
+   * Расположение изображения по оси `x`.
+   */
+  align?: AlignImage;
+  /**
+   * HTML-якорь.
+   */
+  anchor?: string;
+  /**
+   * Подпись.
+   */
+  caption?: string;
+  /**
+   * Дополнительный класс.
+   */
+  className?: string | classNames.ArgumentArray;
+  AnimatedImageProps?: Omit<IAnimatedImageProps, ImagePropsExtends>;
+} & Pick<IAnimatedImageProps, ImagePropsExtends> &
+  Omit<HTMLAttributes<HTMLDivElement>, "className">;
+
+export const Image: FC<ImageProps> = ({
+  align,
+  anchor,
+  caption,
+  alt,
+  src,
+  width,
+  height,
+  blurDataURL,
+  className,
+  AnimatedImageProps,
+  ...props
+}) => {
+  const [isOpen, setToggle] = useToggle(false);
+  const captionOrAlt = caption || alt;
+
+  return (
+    <div
+      id={anchor || undefined}
+      className={classNames(classes.root, className)}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}>
+      <ImageViewer open={isOpen} onClose={setToggle}>
+        <ImageViewerHeader onClose={setToggle} />
+        <ImageViewerBody>
+          <ImageViewerFigure
+            className={classes.imageWrapper}
+            aspectRatio={width / height}
+            figcaptionText={captionOrAlt}>
+            <AnimatedImage
+              alt={alt}
+              src={src}
+              width={width}
+              height={height}
+              sizes="100vw"
+              loading="lazy"
+              blurDataURL={blurDataURL}
+            />
+          </ImageViewerFigure>
+        </ImageViewerBody>
+      </ImageViewer>
+
+      <div className={classes[`align_${align}`]}>
+        <Figure>
+          <ImageViewerButton onClick={setToggle}>
+            <AnimatedImage
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...AnimatedImageProps}
+              alt={alt}
+              src={src}
+              width={width}
+              height={height}
+              sizes="100vw"
+              className={classes.image}
+              loading="lazy"
+              blurDataURL={blurDataURL}
+            />
+          </ImageViewerButton>
+          {captionOrAlt && (
+            <FigureFigcaption text={captionOrAlt} position="outside" />
+          )}
+        </Figure>
+      </div>
+    </div>
+  );
+};
--- components/blocks/Image/utils/imageGQL.ts
+++ components/blocks/Image/utils/imageGQL.ts
@@ -1,11 +1,32 @@
 import { gql } from "@apollo/client";
 
+import { AlignImage } from "../../utils/types";
+
 export type ImageBlockGQLAttributes = {
   id: number;
-  align: string;
+  /**
+   * Расположение изображения по оси `x`.
+   */
+  align: "" | AlignImage;
+  /**
+   * HTML-якорь.
+   */
+  anchor: string;
+  /**
+   * Подпись.
+   */
   caption: string;
+  /**
+   * Альтернативный текст.
+   */
   alt: string;
+  /**
+   * Дополнительный класс.
+   */
   className: string;
+  /**
+   * Ссылка на изображение.
+   */
   url: string;
 };
 
@@ -15,10 +36,12 @@ export const imageBlockGQL = {
   fragments: gql`
     fragment imageBlockGQL on CoreImageBlock {
       ... on CoreImageBlock {
+        name
         attributes {
           ... on CoreImageBlockAttributes {
             id
             align
+            anchor
             caption
             alt
             className
