d2f187b77d50d927925f4f5f553a98b711a0e8da
Timofei <FRedeaX@Yandex.ru>
2023-07-26 16:13:25 +0500

    Добавляет типы `Category`

---
+++ components/Posts/Category/Category.tsx
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+import Link from "next/link";
+import { FC } from "react";
+
+import { Scroller } from "../../Scroller/Scroller";
+
+import classes from "./Category.module.css";
+
+export type CategoryProps = {
+  data: {
+    id: string;
+    uri?: string;
+    name: string;
+  }[];
+  className?: string;
+};
+
+export const Category: FC<CategoryProps> = ({ data, className }) => (
+  <div className={classes.block}>
+    <Scroller>
+      {data.map(({ id, uri, name }) =>
+        uri ? (
+          <Link key={id} href={uri} prefetch={false}>
+            <a className={classNames(classes.link, className)}>
+              <span className={classes.text}>{name}</span>
+            </a>
+          </Link>
+        ) : (
+          <span
+            key={id}
+            className={classNames(
+              classes.link,
+              classes["link--cursor"],
+              className,
+            )}>
+            {name}
+          </span>
+        ),
+      )}
+    </Scroller>
+  </div>
+);
