fea09cc5273b2d066c88792cb932aed6a066f7d4
Timofei <FRedeaX@Yandex.ru>
2023-08-03 10:31:10 +0500

    Отключает анкетирование

--- pages/questionnaire/index.tsx
+++ pages/questionnaire/index.tsx
@@ -1,11 +1,13 @@
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 
-import { getMenu } from "@/core/backend";
+import { getMenu } from "@/core/ssr";
 import { exceptionLog } from "@/helpers";
 import { staticNotFound } from "@/helpers/backend";
 import { RouteQuestionnaire } from "@/routes/Questionnaire";
-import Head from "@/components/Head/Head";
-import Layout from "@/components/UI/Layout/Layout";
+import { SEO } from "@/components/SEO/SEO";
+import { Layout } from "@/components/UI/Layout/Layout";
+
+export const questionnaireIsCompleted = true;
 
 type GetStaticPropsResult = {
   menu: Awaited<ReturnType<typeof getMenu>>;
@@ -15,6 +17,10 @@ export const getStaticProps: GetStaticProps<
   GetStaticPropsResult
 > = async () => {
   try {
+    if (questionnaireIsCompleted) {
+      return staticNotFound;
+    }
+
     const menu = await getMenu();
 
     return {
@@ -31,8 +37,8 @@ export const getStaticProps: GetStaticProps<
 type QuestionnairePageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const QuestionnairePage: NextPage<QuestionnairePageProps> = ({ menu }) => (
-  <Layout menu={menu} paddingSides={15}>
-    <Head title="Анкета пользователя Библиотеки Модельного стандарта" />
+  <Layout menu={menu}>
+    <SEO title="Анкета пользователя Библиотеки Модельного стандарта" />
     <RouteQuestionnaire />
   </Layout>
 );
