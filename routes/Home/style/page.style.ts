a1747534a0fc397d60c42f0370fdcb2f4b065812
Timofei <FRedeaX@Yandex.ru>
2023-08-17 11:15:31 +0500

    Исправляет 4192123

--- routes/Home/style/page.style.ts
+++ routes/Home/style/page.style.ts
@@ -9,10 +9,6 @@ export const sxSection: ContainerProps["sx"] = {
   "--card-margin-x": "calc(var(--gap) / 2)",
   "--card-margin-y": "calc(var(--gap) / 2)",
 
-  "@media (max-width: 652px)": {
-    "--card-margin-x": "auto",
-  },
-
   "@media (min-width: 1013px)": {
     "--carousel-button-left": "-28px",
     "--carousel-button-right": "-28px",
@@ -20,6 +16,7 @@ export const sxSection: ContainerProps["sx"] = {
 
   display: "flex",
   flexWrap: "wrap",
+  justifyContent: "center",
   paddingY: "var(--gap)",
   paddingX: "calc(var(--gap) / 2)",
 };
