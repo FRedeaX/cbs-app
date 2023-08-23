9fda6f741093b28356a03cc8f5ca481c07c30232
Timofei <FRedeaX@Yandex.ru>
2023-08-23 11:41:57 +0500

    Добавляет динамический импорт для `CircularProgress`

--- components/UI/Layout/Layout.tsx
+++ components/UI/Layout/Layout.tsx
@@ -1,10 +1,17 @@
-import { CircularProgress } from "@mui/material";
+import dynamic from "next/dynamic";
 import { FC, ReactNode } from "react";
 
 import { Footer } from "@/components/Footer/Footer";
 import { Header, HeaderProps } from "@/components/Header/Header";
 
 import classes from "./Layout.module.css";
+
+const DynamicCircularProgress = dynamic(
+  () => import("@mui/material/CircularProgress"),
+  {
+    ssr: true,
+  },
+);
 
 type LayoutProps = {
   menu: HeaderProps["menus"];
@@ -25,7 +32,7 @@ export const Layout: FC<LayoutProps> = ({
     <main className={classes.main}>
       {pageLoading ? (
         <div className={classes.loader}>
-          <CircularProgress />
+          <DynamicCircularProgress />
         </div>
       ) : (
         children
