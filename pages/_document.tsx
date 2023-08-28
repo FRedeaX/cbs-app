629836ebc066f1e833da6cc1c313a411f7ed1920
Timofei <FRedeaX@Yandex.ru>
2023-08-28 09:27:07 +0500

    Уменьшает цепочку запросов к google.fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,40 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+import { roboto, robotoSlab } from "@/styles/font";
+
 class _Document extends Document {
   render() {
     return (
       <Html lang="ru">
         <Head>
-          {/* {this.props.styles} */}
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap"
             rel="stylesheet"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;500;600;700&display=swap"
             rel="stylesheet"
-          />
+          /> */}
+          <style>{roboto}</style>
+          <style>{robotoSlab}</style>
+
           <link rel="preconnect" href="https://mc.yandex.ru/" />
-          <link rel="preconnect" href="https://www.googletagmanager.com/" />
 
           <link rel="shortcut icon" href="/logos/favicon.ico" />
+          <link rel="shortcut icon" href="/logos/logo@16.png" sizes="16x16" />
+          <link rel="shortcut icon" href="/logos/logo@132.png" sizes="32x32" />
+          <link
+            rel="shortcut icon"
+            href="/logos/logo@192.png"
+            sizes="192x192"
+          />
+
           <link
             rel="apple-touch-icon"
             href="/logos/logo@76.png"
@@ -39,14 +56,6 @@ class _Document extends Document {
             sizes="180x180"
           />
 
-          <link rel="shortcut icon" href="/logos/logo@16.png" sizes="16x16" />
-          <link rel="shortcut icon" href="/logos/logo@132.png" sizes="32x32" />
-          <link
-            rel="shortcut icon"
-            href="/logos/logo@192.png"
-            sizes="192x192"
-          />
-
           <meta name="theme-color" content="#f2f2f2" />
           <meta name="yandex-verification" content="b761a7a26da38d4e" />
         </Head>
@@ -58,41 +67,5 @@ class _Document extends Document {
     );
   }
 }
-
-// _Document.getInitialProps = async (ctx) => {
-//   const originalRenderPage = ctx.renderPage;
-
-//   // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
-//   // However, be aware that it can have global side effects.
-//   const cache = createEmotionCache();
-//   const { extractCriticalToChunks } = createEmotionServer(cache);
-
-//   /* eslint-disable */
-//   ctx.renderPage = () =>
-//     originalRenderPage({
-//       enhanceApp: (App) =>
-//         function EnhanceApp(props) {
-//           return <App emotionCache={cache} {...props} />;
-//         },
-//     });
-//   /* eslint-enable */
-
-//   const initialProps = await Document.getInitialProps(ctx);
-
-//   const emotionStyles = extractCriticalToChunks(initialProps.html);
-//   const emotionStyleTags = emotionStyles.styles.map((style) => (
-//     <style
-//       data-emotion={`${style.key} ${style.ids.join(" ")}`}
-//       key={style.key}
-//       // eslint-disable-next-line react/no-danger
-//       dangerouslySetInnerHTML={{ __html: style.css }}
-//     />
-//   ));
-
-//   return {
-//     ...initialProps,
-//     styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
-//   };
-// };
 
 export default _Document;
---
+++ styles/font/index.ts
@@ -0,0 +1,2 @@
+export * from "./roboto";
+export * from "./robotoSlab";
