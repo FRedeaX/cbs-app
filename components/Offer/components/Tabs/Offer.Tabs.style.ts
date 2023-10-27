6859e523ffeb9e10973bf5e88047a6f9fd387df6
Timofei <FRedeaX@Yandex.ru>
2023-10-27 15:50:15 +0500

    Добавляет `Card` в `Offer`

--- components/Offer/components/Tabs/Offer.Tabs.style.ts
+++ components/Offer/components/Tabs/Offer.Tabs.style.ts
@@ -2,11 +2,18 @@ import { TabPanelProps } from "@mui/lab";
 import { BoxProps, ContainerProps, TabProps } from "@mui/material";
 
 export const sxOfferRoot: ContainerProps["sx"] = {
+  "--transform-translate": `6px`,
+
+  "--is-font-size-adaptive": 0,
+  "--carousel-offset-y": `var(--transform-translate)`,
+  "--carousel-offset-x": `5px`,
+  "--card-width": "calc(288px)",
   "--card-margin-x": "5px",
-  "--card-width": "calc(288px + var(--card-margin-x) * 2)",
-  "--card-info-border-right": "1px solid var(--black-10)",
-  "--card-info-border-bottom": "1px solid var(--black-10)",
-  "--card-info-border-left": "1px solid var(--black-10)",
+  "--card-margin-y": "10px",
+  "--card-transform-translate-y": `calc(var(--transform-translate) * -1)`,
+  "--card-content-border-width": `0 1px 1px`,
+  "--card-content-border-color": `var(--black-10)`,
+  "--card-content-border-style": `solid`,
 
   marginTop: "var(--offer-margin-top)",
   marginBottom: "var(--offer-margin-bottom)",
@@ -27,3 +34,7 @@ export const sxOfferHeader: BoxProps["sx"] = {
 export const sxOfferTab: TabProps["sx"] = { whiteSpace: "pre-line" };
 
 export const sxOfferTabPanel: TabPanelProps["sx"] = { padding: 0 };
+
+export const sxScroller: BoxProps["sx"] = {
+  paddingX: `var(--card-margin-x)`,
+};
