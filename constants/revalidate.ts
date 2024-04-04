fe52d9c55ae94f3ac09c13690afbfbe14fa95867
Timofei <FRedeaX@Yandex.ru>
2024-04-04 11:43:41 +0500

    Удаляет неиспользуемые переменные

--- constants/revalidate.ts
+++ constants/revalidate.ts
@@ -1,10 +1,1 @@
-/**
- * Инкрементная статическая регенерация (ISR)
- */
-export const POST = 600;
-/**
- * Инкрементная статическая регенерация (ISR)
- */
-export const PAGE = 600;
-
 export const PREVIEW_POLL_INTERVAL = 10000;
