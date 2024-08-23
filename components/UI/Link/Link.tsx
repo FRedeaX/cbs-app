e84c8cac447f46384ec0a40a2d2a9858cc88cc4e
Timofei <FRedeaX@Yandex.ru>
2024-08-23 12:08:08 +0500

    Исправление (PaginationLink): прокидывает `ref`

--- components/UI/Link/Link.tsx
+++ components/UI/Link/Link.tsx
@@ -1,16 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Link as MUILink, LinkProps as MUILinkProps } from "@mui/material";
 import NextLink, { LinkProps as NextLinkProps } from "next/link";
-import { FC, forwardRef } from "react";
+import { forwardRef } from "react";
 
 type NJSLinkProps = Pick<
   NextLinkProps,
   "href" | "replace" | "scroll" | "shallow" | "prefetch"
 >;
-export type LinkProps = Omit<MUILinkProps, keyof NJSLinkProps | "ref"> &
-  NJSLinkProps;
+export type LinkProps = Omit<MUILinkProps, keyof NJSLinkProps> & NJSLinkProps;
 
-export const Link: FC<LinkProps> = forwardRef((props, ref) => (
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
   <MUILink component={NextLink} ref={ref} {...props} />
 ));
 
