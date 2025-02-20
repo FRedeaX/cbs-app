7fe3b5fe7526db687675efdac72786e4d2647eef
Timofei <FRedeaX@Yandex.ru>
2025-02-20 09:14:55 +0500

    [Social] Обновляет брендовые цвета сторонних сервисов

--- components/Share/Share.jsx
+++ components/Share/Share.jsx
@@ -7,7 +7,6 @@ import classes from "./Share.module.css";
 const Share = ({ cls, clsLink, title, href, image }) => {
   let urlVK = `https://vk.com/share.php?url=${href}`;
   let urlOK = `https://connect.ok.ru/offer?url=${href}`;
-  let urlFB = `https://www.facebook.com/sharer/sharer.php?u=${href}`;
 
   // let url = baseUrl;
   // if (title) url = `${url}&title=${title}`;
@@ -20,14 +19,10 @@ const Share = ({ cls, clsLink, title, href, image }) => {
     urlOK += `&title=${encodeURIComponent(
       `${title} | Библиотеки города Байконур`,
     )}`;
-    urlFB += `&title=${encodeURIComponent(
-      `${title} | Библиотеки города Байконур`,
-    )}`;
   }
   if (image) {
     urlVK += `&image=${encodeURIComponent(image)}`;
     urlOK += `&imageUrl=${encodeURIComponent(image)}`;
-    // urlFB += `&image=${encodeURIComponent(image)}`;
   }
 
   return (
@@ -35,7 +30,6 @@ const Share = ({ cls, clsLink, title, href, image }) => {
       {/* <div className={classes.hover}> */}
       <Social type="vk" url={urlVK} clsLink={clsLink} clsSVG={classes.svg} />
       <Social type="ok" url={urlOK} clsLink={clsLink} clsSVG={classes.svg} />
-      <Social type="fb" url={urlFB} clsLink={clsLink} clsSVG={classes.svg} />
       {/* </div> */}
     </div>
   );
